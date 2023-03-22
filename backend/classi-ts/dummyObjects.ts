bf7d8969f6238cdeb902f784e25381ee2e22545b
Alexbonny28 <al.bonomo@studenti.unina.it>
2023-03-22 17:25:48 +0100

    aggiunto docker file e sistemato script di creazione db
    devo aggiungere al docker la creazione automatica del db se non esiste e lanciare il  frontend dopo il backend

--- backend/classi-ts/dummyObjects.ts
+++ backend/classi-ts/dummyObjects.ts
@@ -1,9 +1,10 @@
 import { Ordinazione } from './ordinazione';
 import { Ristorante } from './ristorante';
-
+import { Cameriere } from './utente';
+/*
 const dummyResturant = new Ristorante("Ristorante", "Via Roma", "1234567890", "www.ristorante.it", "img/ristorante.jpg", { camerieri: [], addettiAllaCucina: [] });
 
-const dummyOrdinazione = new Ordinazione(1, new Date(), [], false, []);
+const dummyOrdinazione = new Ordinazione(1, new Date(), Cameriere[], false, []);
 
 
 export { 
@@ -11,3 +12,4 @@ export {
         dummyOrdinazione
         
     };
+    */
